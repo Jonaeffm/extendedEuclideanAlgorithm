219ab6409dc6a207f1a82cf75572d5d2c8b004a1
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2021-11-03 21:12:29 +0100

    Add files via upload

---
+++ extendedEuclideanAlgorithm/src/extendedEuclideanAlgorithm/Line.java
@@ -0,0 +1,48 @@
+package extendedEuclideanAlgorithm;
+
+public class Line {
+
+	private int a;
+	private int b;
+	private int q;
+	private int r;
+	private int x;
+	private int y;
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+	public int getQ() {
+		return q;
+	}
+	public void setQ(int q) {
+		this.q = q;
+	}
+	public int getR() {
+		return r;
+	}
+	public void setR(int r) {
+		this.r = r;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+}
